5a5566d568cef59f5a5e44085cf0dbd7767b2d78
Negrea-Andrei <negrea97@yahoo.com>
2024-03-28 20:43:21 +0200

    add the css for layout

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Lorem ipsum</h1>
+      <Link href="/">Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </Link>
+    </div>    
+  );
+}
