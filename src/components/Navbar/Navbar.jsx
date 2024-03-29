252cc03f21639e09bdc3e917a61f2e4f72c0d83c
Negrea-Andrei <negrea97@yahoo.com>
2024-03-29 20:25:37 +0200

    added file for the toggle of the screen colors

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import style from "./navbar.module.css"
 import React from 'react'
+import test from "../../../public/next.svg"
+import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div className={style.container}>Navbar</div>
+    <div className={style.container}>
+      <div className={style.social}>
+        <Image src ={test} alt="socials" width={24} height={23} />
+        <Image src ={test} alt="socials" width={24} height={23} />
+        <Image src ={test} alt="socials" width={24} height={23} />
+      </div>
+      <div className={style.logo}>GOOD NEWS</div>
+      <div className={style.links}>
+        <Link href="/">Homepage</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">About</Link>
+        <Link href="/">Login</Link>
+      </div>
+    </div>
   )
 }
 
