73150a282e7e746d2c6c282b8e36332328792549
Negrea-Andrei <negrea97@yahoo.com>
2024-03-30 21:40:26 +0200

    created a theme context and a provider so that i can change the theme globally on the website

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,14 +1,33 @@
+"use client"
+
 import style from "./themeToggle.module.css"
 import moon from "../../../public/moon.png"
 import sun from "../../../public/sun.png"
 import Image from "next/image"
 import React from 'react'
+import { useContext } from "react"
+import { ThemeContext } from "@/context/ThemeContext"
 
 const ThemeToggle = () => {
+  const { toggle, theme } = useContext(ThemeContext);
+
   return (
-    <div className={style.container}>
+    <div
+      className={style.container}
+      onClick={toggle}
+      style={
+        theme === "dark" ? { backgroundColor: "white" } : { backgroundColor: "#0f172a" }
+      }
+    >
       <Image src={moon} alt="moon" width={14} height={14} />
-      <div className={style.ball}></div>
+      <div
+        className={style.ball}
+        style={
+          theme === "dark"
+            ? { left: 1, background: "#0f172a" }
+            : { right: 1, background: "white" }
+        }
+      ></div>
       <Image src={sun} alt="moon" width={14} height={14} />
     </div>
   )
