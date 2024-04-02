a42ca40054841ae362e3d633f644b97494ee73d2
Negrea-Andrei <negrea97@yahoo.com>
2024-04-02 19:41:47 +0300

    create the card component and the side menu for posts

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,24 +1,43 @@
-import PagesComponent from "../PagesComponent/PagesComponent"
-import style from "./cardList.module.css"
-import React from 'react'
-import Image from "next/image"
+import React from "react";
+import styles from "./cardList.module.css";
+import Pagination from "../pagination/Pagination";
+import Image from "next/image";
+import Card from "../card/Card";
 
-const CardList = () => {
+const getData = async (page, cat) => {
+  const res = await fetch(
+    `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const CardList = async ({ page, cat }) => {
+  const { posts, count } = await getData(page, cat);
+
+  const POST_PER_PAGE = 2;
+
+  const hasPrev = POST_PER_PAGE * (page - 1) > 0;
+  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
+
   return (
-    <div className={style.container}>
-      <h1 className={style.title}>Recent Posts</h1>
-      <div className={style.posts}>
-        <div className={style.post}>
-          <div className={style.imageContainer}>
-            <Image src="/p1.jpeg" alt="autofill" fill/>
-          </div>
-          <div className={style.textContainer}></div>
-          
-        </div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        {posts?.map((item) => (
+          <Card item={item} key={item._id} />
+        ))}
       </div>
-      <PagesComponent />
+      <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} />
     </div>
-  )
-}
+  );
+};
 
-export default CardList
+export default CardList;
