a1e3fbfe7c5b6b9c5102246c6d73107442546a46
Negrea-Andrei <negrea97@yahoo.com>
2024-04-02 20:51:56 +0300

    added a component for when selecting a category

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,20 @@
+import CardList from "@/components/cardList/CardList";
+import style from "./blogPage.module.css";
+import Menu from "@/components/Menu/Menu";
+
+const BlogPage = ({ searchParams }) => {
+  const page = parseInt(searchParams.page) || 1;
+  const { cat } = searchParams;
+
+  return (
+    <div className={style.container}>
+      <h1 className={style.title}>{cat} Blog</h1>
+      <div className={style.content}>
+        <CardList page={page} cat={cat}/>
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default BlogPage;
