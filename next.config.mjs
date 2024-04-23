373f3a6968287a2727b89634e9652b6d38afafc8
Andrei Negrea <negrea97@yahoo.com>
2024-04-23 08:29:49 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
-// next.config.mjs
-
 const nextConfig = {
     images: {
-        remotePatterns: [
-            { domain: 'lh3.googleusercontent.com', path: '/.*' },
-            { domain: 'firebasestorage.googleapis.com', path: '/.*' }
-        ]
+        domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"]
     }
-};
-
-export default nextConfig;
+}
