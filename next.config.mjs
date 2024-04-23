a9ffccaee5f35677fcea5b004d09a3e6aad8e1fe
Andrei Negrea <negrea97@yahoo.com>
2024-04-23 08:26:27 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"]
+        remotePatterns: [
+            'https://lh3\\.googleusercontent\\.com/.*',
+            'https://firebasestorage\\.googleapis\\.com/.*'
+        ]
     }
 }
 
